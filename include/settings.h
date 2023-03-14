33fe20cba5245dcda68f095b86f147acff8ef2a9
steve D <66957638+NPC-lab-python@users.noreply.github.com>
2023-03-14 16:08:23 +0100

    init

---
+++ include/settings.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+
+// NDE fixe
+IPAddress server(192,168,0,220);
+
+const char * SSID ="maison";
+const char * pass = "galileo@9493!";
+
+// NDE fixe
+IPAddress ip_device(192,168,0,155);
+
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+//IPAddress dns(192,168,0,1);
+String Hostname_wifi = "002PS01";
+
+String clientId = "002PS01";
+String id_mqtt = "cage1_porte_social1";
+String pass_mqtt = "steve";
+
+// OTA
+String HostnameOTA = "002PS01";
+String PasswordOTA = "steve";
+
+
---
+++ src/main.cpp
@@ -0,0 +1,197 @@
+#include <Arduino.h>
+#include <string>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+#include <settings.h>
+#include <Servo.h>
+
+WiFiClient porte;
+PubSubClient MQTTclient(porte);
+Servo PorteSocial;
+//#define START_DEGREE_VALUE 0 // The degree value written to the servo at time of attach.
+
+void onConnected(const WiFiEventStationModeConnected &event);
+void onGotIP(const WiFiEventStationModeGotIP &event);
+void callback(char *topic, byte *payload, unsigned int length);
+
+void MQTTreceive();
+void MQTTconnect();
+void moving();
+void initmoving();
+
+
+// put your setup code here, to run once:
+
+const int led_connect_broker = 13;  // d7
+const int led_connection_wifi = 15; // d8
+const int servopin = 12;            // D6
+int angle = 0;
+
+void setup()
+{
+
+    Serial.begin(9600);
+    Serial.println("hello");
+    // nom de l'objet OTA
+    ArduinoOTA.setHostname(HostnameOTA.c_str());
+    ArduinoOTA.setPassword(PasswordOTA.c_str());
+    WiFi.mode(WIFI_STA);
+    WiFi.softAP("porte social");
+    // config ip fixe
+    WiFi.config(ip_device, gateway, subnet);
+    WiFi.hostname(Hostname_wifi.c_str());
+    WiFi.begin(SSID, pass);
+
+    // démarrage de la connection ; création d'un event pour la connection
+    static WiFiEventHandler onConnectionHandler = WiFi.onStationModeConnected(onConnected);
+    static WiFiEventHandler onGotIPHandler = WiFi.onStationModeGotIP(onGotIP);
+
+    pinMode(led_connection_wifi, OUTPUT);
+    pinMode(led_connect_broker, OUTPUT);
+    MQTTclient.setServer(server, 1883);
+    MQTTclient.setCallback(callback);
+    MQTTclient.setKeepAlive(5);
+
+    Serial.print(F("Attach servo at pin "));
+    Serial.println(servopin);
+    PorteSocial.attach(servopin);
+    initmoving();
+}
+
+void loop()
+{
+    // connect serveur MQTT
+    if (!MQTTclient.connected())
+    {
+        MQTTconnect();
+    }
+
+    if (MQTTclient.loop())
+    {
+    }
+    else
+    {
+        Serial.println("Déconnecté");
+    }
+
+    // televersement à distance
+    ArduinoOTA.handle();
+}
+void initmoving()
+{
+    Serial.println("move");
+
+    // move from 180 to 0 degrees with a negative angle of 5
+    for (angle = 90; angle >= 1; angle -= 1)
+    {
+        PorteSocial.write(angle);
+        delay(100);
+    }
+
+    delay(1000);
+    Serial.println("end move");
+}
+
+void moving()
+{
+    Serial.println("move");
+
+    for (angle = 0; angle < 180; angle += 1)
+    {
+        PorteSocial.write(angle);
+        delay(100);
+    }
+    Serial.println("wait 5 sec");
+
+    delay(10000);
+
+    // move from 180 to 0 degrees with a negative angle of 5
+    for (angle = 180; angle >= 1; angle -= 1)
+    {
+        PorteSocial.write(angle);
+        delay(100);
+    }
+
+    delay(1000);
+    Serial.println("end move");
+
+    MQTTclient.publish("002/feeders/002PS01/", "OK");
+}
+
+void onConnected(const WiFiEventStationModeConnected &event)
+{
+    if (WiFi.isConnected())
+    {
+        Serial.println("Wifi connecté");
+        ArduinoOTA.begin();
+        digitalWrite(led_connection_wifi, HIGH);
+    }
+    else
+    {
+        Serial.println("Not Connected");
+    }
+}
+
+void onGotIP(const WiFiEventStationModeGotIP &event)
+{
+    Serial.println("Connection Ip : " + WiFi.localIP().toString());
+    Serial.println("puissance du signal : ");
+    Serial.println(WiFi.RSSI());
+}
+
+void MQTTreceive()
+{
+    MQTTclient.subscribe("002/feeders/002PS01/set", 1);
+    MQTTclient.publish("devices/connected/002PS01", "True", true);
+    MQTTclient.publish("devices/list/002PS01", "002/feeders/002PS01/", true);
+}
+
+void MQTTconnect()
+{
+    while (!MQTTclient.connected())
+    {
+        Serial.print("Attente  MQTT connection...");
+        Serial.println(clientId.c_str());
+
+        if (MQTTclient.connect(clientId.c_str(), "devices/connected/002PS01", 1, false, "Dirty"))
+        {
+            Serial.println("connecté au server MQTT");
+            Serial.println(server.toString());
+            digitalWrite(led_connect_broker, HIGH);
+            MQTTreceive();
+        }
+
+        else
+        { // si echec affichage erreur
+            Serial.print("ECHEC, rc=");
+            Serial.print(MQTTclient.state());
+            Serial.println(" nouvelle tentative dans 5 secondes");
+            digitalWrite(led_connect_broker, LOW);
+            delay(5000);
+        }
+    }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+
+    payload[length] = '\0';
+    String message((char *)payload);
+
+    Serial.println(message);
+    Serial.println(topic);
+
+    if (strcmp(topic, "002/feeders/002PS01/set") == 0)
+    {
+        Serial.println("Dans topic 002/feeders/002PS01/set");
+        if ((char)payload[0] == '1')
+        {
+            moving();
+        }
+        if ((char)payload[0] == '2')
+        {
+            moving();
+        }
+    }
+}
